cedcca7192f1844078a4db4c6d276397c9aa6ddc
Marcus Weiner <marcus.weiner@gmail.com>
2024-01-20 17:26:02 +0100

    Create VMs for supernodes

--- terraform/modules/supernode/main.tf
+++ terraform/modules/supernode/main.tf
@@ -3,5 +3,11 @@ terraform {
     netbox = {
       source = "e-breuninger/netbox"
     }
+    proxmox = {
+      source = "Telmate/proxmox"
+      # newer versions have issues with granular permission on a token
+      # https://github.com/Telmate/terraform-provider-proxmox/issues/784
+      version = "<= 2.9.11"
+    }
   }
 }
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,9 +4,17 @@ terraform {
       source  = "e-breuninger/netbox"
       version = "3.7.6"
     }
+    proxmox = {
+      source  = "Telmate/proxmox"
+      version = "2.9.11"
+    }
   }
 }
 
 provider "netbox" {
   server_url = "https://netbox.freifunk-duesseldorf.de"
+}
+
+provider "proxmox" {
+  pm_api_url = "https://pve.freifunk-duesseldorf.de/api2/json"
 }
