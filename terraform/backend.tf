77f1731b4fc47f691df01c448cda561c4cd494fd
Marcus Weiner <marcus.weiner@gmail.com>
2024-09-02 11:17:52 +0200

    Migrate to self-hosted backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,11 +1,8 @@
 terraform {
   backend "http" {
-    # see https://tfstate.dev/ for instructions
-    address        = "https://api.tfstate.dev/github/v1"
-    lock_address   = "https://api.tfstate.dev/github/v1/lock"
-    unlock_address = "https://api.tfstate.dev/github/v1/lock"
-    lock_method    = "PUT"
-    unlock_method  = "DELETE"
-    username       = "ffddorf/supernodes-v2@dev"
+    address        = "https://ffddorf-terraform-backend.fly.dev/state/supernodes-v2/dev"
+    lock_address   = "https://ffddorf-terraform-backend.fly.dev/state/supernodes-v2/dev"
+    unlock_address = "https://ffddorf-terraform-backend.fly.dev/state/supernodes-v2/dev"
+    username       = "github_pat"
   }
 }
